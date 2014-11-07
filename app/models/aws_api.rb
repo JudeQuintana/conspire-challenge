aee0a0d29b8e10be4a767e037082bc8773cae865
Jude Quintana <jude.quintana@gmail.com>
2014-11-07 16:21:06 -0700

    compacting code

--- app/models/aws_api.rb
+++ app/models/aws_api.rb
@@ -15,24 +15,18 @@ class AwsApi
 
   def connection
     bucket_name = ENV["S3_BUCKET"]
-
     AWS.config(:access_key_id => ENV["AMAZON_ACCESS_KEY_ID"],
                :secret_access_key => ENV["AMAZON_SECRET_ACCESS_KEY"])
-
     s3 = AWS::S3.new
     s3.buckets[bucket_name]
   end
 
   def build_objects_arr
     @bucket.objects.each_with_object([]) { |obj, arr|
-
       separated_elements = parse_tabs(obj.read)
-
       separated_elements.each { |contents|
-
         arr << {"filename" => obj.key, "key" => contents.first.strip, "value" => contents.last.strip}
       }
     }
   end
-
 end
--- app/models/sorter.rb
+++ app/models/sorter.rb
@@ -30,5 +30,4 @@ class Sorter
         "filename"
     end
   end
-
 end
