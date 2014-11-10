2146a8975d5bc927518f26f875c4c02f098b973d
Jude Quintana <jude.quintana@gmail.com>
2014-11-09 17:05:10 -0700

    added api spec testing /query endpoint with search params

---
+++ spec/api/api_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+
+describe 'get /query' do
+
+  let(:partial_expected) {
+    {"filename" => "claims_to_fame.txt", "key" => "Boulder, CO", "value" => "mountains and marijuana"}
+  }
+  it 'tests /query endpoint returns a json blob' do
+
+    get '/query'
+
+
+    expect(response.code.to_i).to eq 200
+    expect(JSON.parse(response.body)).to include(partial_expected)
+
+  end
+
+  it 'test /query endpoint with search params' do
+    get '/query?sort=fkv'
+
+    expect(response.code.to_i).to eq 200
+    expect(JSON.parse(response.body)).to include(partial_expected)
+  end
+
+end
